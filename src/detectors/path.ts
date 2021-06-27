4c5a38cef83ad2347d9e6cf6031f212961301111
Shelton Suen <sheltonsuen@deepstudy.tech>
2021-06-27 23:13:56 +0800

    feat(#1): in case window are not defined

--- src/detectors/path.ts
+++ src/detectors/path.ts
@@ -5,7 +5,7 @@ const LANG_PATH_SEGMENT = /\/([a-zA-Z-]*)/g;
 const PathDetector: Detector = {
   name: 'path',
   lookup(options) {
-    const langSegments = window.location.pathname.match(LANG_PATH_SEGMENT);
+    const langSegments = window?.location?.pathname?.match(LANG_PATH_SEGMENT);
     if (!langSegments) {
       return undefined;
     }
